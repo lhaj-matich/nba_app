3be58ce7906cde7a983fed29537b0f7243de7a72
Choumoul Oussama <pentestossa@gmail.com>
2021-08-23 18:42:45 +0100

    Update firebase.js
    Some minor changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,9 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAg3xYN8tCT0c5EGNJxjjLQIx2y81lzHEQ",
     authDomain: "nbafull-2d84b.firebaseapp.com",
-    databaseURL: "https://nbafull-2d84b.firebaseio.com",
     projectId: "nbafull-2d84b",
     storageBucket: "nbafull-2d84b.appspot.com",
-    messagingSenderId: "758479197734",
-    appId: "1:758479197734:web:75e5ef3cb695f8ff1678e7"
   };
 
 firebase.initializeApp(firebaseConfig);
